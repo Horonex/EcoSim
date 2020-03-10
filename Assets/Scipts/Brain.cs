ed4e104d329f1df6004b495a4e269d4edb7123c8
Adrien Jeanson <Adrilus3000@live.ca>
2020-03-09 22:37:03 -0400

    brain

--- Assets/Scipts/Brain.cs
+++ Assets/Scipts/Brain.cs
@@ -5,6 +5,8 @@ using System.Text;
 using UnityEngine;
 using System.Threading.Tasks;
 
+
+
 namespace Assets.Scipts
 {
     class Brain
@@ -13,23 +15,41 @@ namespace Assets.Scipts
 
         int hiddenLayers;
 
-        double[,] inputNodes;
-        string[] inputTypes;
-        double[][,] hiddenNodes;
-        double[,] outputNodes;
+        float[,] inputNodes;
+        Dictionary<string, int> Genes;
+        float[][,] hiddenNodes;
+        float[,] outputNodes;
 
-        double[,] weightIH;
-        double[][,] weightHH;
-        double[,] weightHO;
+        float[,] weightIH;
+        float[][,] weightHH;
+        float[,] weightHO;
 
-        double[,] biasIH;
-        double[][,] biasHH;
-        double[,] biasHO;
+        float[,] biasIH;
+        float[][,] biasHH;
+        float[,] biasHO;
 
 
 
-        public Brain(Brain parent,float tweakPourcentage)
-        {
+        public Brain(SortedList<Input, int> inputs, Brain parent, float tweakPourcentage=0)
+        {           
+            weightIH = DuplicateArray(parent.weightIH, tweakPourcentage);
+            weightHO = DuplicateArray(parent.weightHO, tweakPourcentage);
+
+            biasIH = DuplicateArray(parent.biasIH, tweakPourcentage);
+            biasHO = DuplicateArray(parent.biasHO, tweakPourcentage);
+
+            weightHH = new float[parent.weightHH.Length][,];
+            for (int i = 0; i < weightHH.Length; i++)
+            {
+                weightHH[i] = DuplicateArray(parent.weightHH[i], tweakPourcentage);
+            }
+                
+            biasHH = new float[parent.biasHH.Length][,];
+            for (int i = 0; i < biasHH.Length; i++)
+            {
+                biasHH[i] = DuplicateArray(parent.biasHH[i], tweakPourcentage);
+            }
+
             //90 small
             //9 big
             //1 random
@@ -42,7 +62,7 @@ namespace Assets.Scipts
         }
 
 
-        public Brain(SortedList<Input,int> inputs)
+        public Brain(SortedList<Input,int> inputs, Brain parent)
         {
             
 
@@ -62,39 +82,39 @@ namespace Assets.Scipts
             //set number of hidden layers
             this.hiddenLayers = hiddenLayers;
             //set length of input nodes
-            inputNodes = new double[inputLenght, 1];
+            inputNodes = new float[inputLenght, 1];
             //set length of hiddens layers nodes
-            hiddenNodes = new double[hiddenLayers][,];
+            hiddenNodes = new float[hiddenLayers][,];
             for (int i = 0; i < hiddenNodes.Length; i++)
             {
-                hiddenNodes[i] = new double[hiddenLenght, 1];
+                hiddenNodes[i] = new float[hiddenLenght, 1];
             }
             //set length of output nodes
-            outputNodes = new double[outputLenght, 1];
+            outputNodes = new float[outputLenght, 1];
 
             //set length weiht of input to hidden and hidden to outupt
-            weightIH = new double[inputLenght, hiddenLenght];
-            weightHO = new double[hiddenLenght, outputLenght];
+            weightIH = new float[inputLenght, hiddenLenght];
+            weightHO = new float[hiddenLenght, outputLenght];
 
             //set length biases of input to hidden and hidden to outupt
-            biasIH = new double[hiddenLenght, 1];
-            biasHO = new double[outputLenght, 1];
+            biasIH = new float[hiddenLenght, 1];
+            biasHO = new float[outputLenght, 1];
 
             //if more than one hidden layer 
             if (hiddenLayers > 1)
             {
                 //set length weihts of hidden to hidden
-                weightHH = new double[hiddenLayers - 1][,];
+                weightHH = new float[hiddenLayers - 1][,];
                 for (int i = 0; i < weightHH.Length; i++)
                 {
-                    weightHH[i] = new double[hiddenLenght, 1];
+                    weightHH[i] = new float[hiddenLenght, 1];
                 }
 
                 //set length biases of hidden to hidden
-                biasHH = new double[hiddenLayers - 1][,];
+                biasHH = new float[hiddenLayers - 1][,];
                 for (int i = 0; i < biasHH.Length; i++)
                 {
-                    biasHH[i] = new double[hiddenLenght, 1];
+                    biasHH[i] = new float[hiddenLenght, 1];
                 }
             }
         }
@@ -130,7 +150,7 @@ namespace Assets.Scipts
             }
         }
 
-        private void FillArray(double[,] array)
+        private void FillArray(float[,] array)
         {
             for(int i=0; i<array.GetLength(0);i++)
             {
@@ -139,6 +159,32 @@ namespace Assets.Scipts
                     array[i, j] = UnityEngine.Random.Range(-0.5f, 0.5f);
                 }
             }
+        }
+
+        private float[,] DuplicateArray(float[,] item)
+        {
+            var output = new double[item.GetLength(0), item.GetLength(1)];
+            for (int i = 0; i < item.GetLength(0); i++)
+            {
+                for (int j = 0 ; j < item.GetLength(0); j++)
+                {
+                    output[i, j] = item[i, j];
+                }
+            }
+            return output;
+        }
+        private float[,] DuplicateArray(float[,] item, float tweakPourcentage)
+        {
+            var output = new double[item.GetLength(0), item.GetLength(1)];
+            for (int i = 0; i < item.GetLength(0); i++)
+            {
+                for (int j = 0; j < item.GetLength(0); j++)
+                {
+                    float rand = UnityEngine.Random.Range(-(tweakPourcentage / 100), tweakPourcentage / 100);
+                    output[i, j] = item[i, j]+rand;
+                }
+            }
+            return output;
         }
 
     }
